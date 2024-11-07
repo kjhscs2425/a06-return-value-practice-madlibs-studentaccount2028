0cbf1203a68b7628058a8256d5ae06c32d9c26e3
studentaccount2028 <tpark2028@kehillahstudent.org>
2024-11-07 11:13:08 -0800

    final

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun: ")
+pluralnoun0 = input("Give me a plural noun: ")
+female = input("Give me the name of a female character: ")
+adjective1 = input("Give me an adjectivce")
+clothing = input("Give me an article of clothing")
+noun1 = input("Give me a noun: ")
+city = input("Give me a city")
+pluralnoun1 = input("Give me a plural noun: ")
+adjective2 = input("Give me an adjective: ")
+adjective3 = input("Give me an adjective: ")
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+
+story = f"There are many {adjective0} ways to choose a/n {noun0} to read. First you can ask for reccomendations from your friends and {pluralnoun0} . Just don't ask {female} - she only reads {adjective1} books with {clothing} - ripping godesses on the cover. If your friends and family are no help try checking out the {noun1} review in The  {city} Times. If the {pluralnoun1} feautured there are too {adjective2} for your taste, try something a little bit more {adjective3}"
 print(story)
